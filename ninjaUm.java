8c21f9982608fbb0d73bcdb62dece2da9d2e4033
João Almeida <93356982+JoaoLima1807@users.noreply.github.com>
2026-01-30 17:17:13 -0300

    Add files via upload

---
+++ ninjaUm.java
@@ -0,0 +1,65 @@
+package desafios;
+
+public class ninjaUm {
+    static void main() {
+
+        System.out.println("----------------------------------------------------------");
+
+        // Ninja 3
+
+        String nomeNinja1 = "Naruto";
+        int idadeNinja1 = 17;
+        String missaoNinja1 = "Recuperar um Cachorro";
+        String statusMissao1 = "Concluida";
+        char nivelMissao1 = 'A';
+        String estadoNinja1 = "Bem";
+
+        System.out.println("O ninja para missão se chama: " + nomeNinja1);
+        System.out.println("A idade do ninja é: " + idadeNinja1);
+        System.out.println("A missão que o nija deve executar é: " + missaoNinja1);
+        System.out.println("O status da missão é: " + statusMissao1);
+        System.out.println("O nivel da missão era: " + nivelMissao1);
+        System.out.println("O estado do ninja " + nomeNinja1 + " é: " + estadoNinja1);
+
+        System.out.println("----------------------------------------------------------");
+
+
+        // Ninja 2
+
+        String nomeNinja2 = "Neji";
+        int idadeNinja2 = 18;
+        String missaoNinja2 = "Espionar o rei";
+        String statusMissao2 = "Em andamento";
+        char nivelMissao2 = 'B';
+        String estadoNinja2 = "Ferido";
+
+        System.out.println("O ninja para missão se chama: " + nomeNinja2);
+        System.out.println("A idade do ninja é: " + idadeNinja2);
+        System.out.println("A missão que o nija deve executar é: " + missaoNinja2);
+        System.out.println("O status da missão é: " + statusMissao2);
+        System.out.println("O nivel da missão era: " + nivelMissao2);
+        System.out.println("O estado do ninja " + nomeNinja2 + " é: " + estadoNinja2);
+
+        System.out.println("----------------------------------------------------------");
+
+        // Ninja 3
+
+        String nomeNinja3 = "Sasuke";
+        int idadeNinja3 = 19;
+        String missaoNinja3 = "Eliminar enimigo";
+        String statusMissao3 = "Fracassada";
+        char nivelMissao3 = 'D';
+        String estadoNinja3 = "Morto";
+
+        System.out.println("O ninja para missão se chama: " + nomeNinja3);
+        System.out.println("A idade do ninja é: " + idadeNinja3);
+        System.out.println("A missão que o nija deve executar é: " + missaoNinja3);
+        System.out.println("O status da missão é: " + statusMissao3);
+        System.out.println("O nivel da missão era: " + nivelMissao3);
+        System.out.println("O estado do ninja " + nomeNinja3 + " é: " + estadoNinja3);
+
+        System.out.println("----------------------------------------------------------");
+
+    }
+
+}
