957bdfba11f49186ec9e09fa3642e30c578b8f08
João Almeida <93356982+JoaoLima1807@users.noreply.github.com>
2026-01-30 18:05:43 -0300

    Add files via upload

---
+++ Condicoes.java
@@ -0,0 +1,30 @@
+public class Condicoes {
+
+    static void main() {
+
+        /*
+            IF e ELSE
+            ELSE IF
+            Objetivo: Paasar o nija de nivel de acordo com o numero de missoes
+         */
+
+        String nome = "João";
+        int idade = 15;
+        boolean hokage = false;
+        short numeroMissoes = 17;
+        String rank;
+
+        if (numeroMissoes >= 10 ) {
+            System.out.println("Rank: Chunnin  ");
+
+        } else if (numeroMissoes >= 20) {
+            System.out.println("Rank: Jounin ");
+
+        } else {
+            System.out.println("Rank: Gennim ");
+        }
+
+
+
+    }
+}
